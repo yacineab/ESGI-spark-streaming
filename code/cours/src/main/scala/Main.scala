eb2934b8a4d0316a998edbc1ad83eb9d37797d31
Yacine A <yacine@MacBook-Pro-de-Yacine.local>
2021-04-14 12:31:31 +0200

    adding json data

--- code/cours/src/main/scala/Main.scala
+++ code/cours/src/main/scala/Main.scala
@@ -5,33 +5,42 @@ object Main extends App {
 
   val spark = SparkSession
     .builder()
+    .config("spark.sql.shuffle.partitions","5")
     .appName("CoursSparkStreaming")
-    .master("local[2]")
+    .master("local[*]")
     .getOrCreate()
 
   spark.sparkContext.setLogLevel("ERROR")
+
   // Create DataFrame
   val staticDataFrame = spark.read.format("csv")
     .option("header", "true")
     .option("inferSchema", "true")
-    .load("../../data/retail-data/by-day/*.csv")
+    .load("../../data/retail-data/by-day/")
+
+  //staticDataFrame.printSchema()
+ // staticDataFrame.show(10, false)
 
   // Create tempTable
   staticDataFrame.createOrReplaceTempView("retail_data")
+ // spark.sql("select InvoiceNo,Description from retail_data").show(false)
 
   // Create staticSchema
-  val staticSchema = staticDataFrame.schema
+   val staticSchema = staticDataFrame.schema
 
   val largePurchase = staticDataFrame
     .selectExpr(
       "CustomerId",
       "(UnitPrice * Quantity) as total_cost", "InvoiceDate")
     .groupBy(
-      col("CustomerId"), window(col("InvoiceDate"), "1 day"))
+      col("CustomerId"), window(col("InvoiceDate"),
+        "1 day","1 day","14 hour"))
     .sum("total_cost")
 
+  // show static dataframe
   //largePurchase.show(false)
 
+  // set default partition conf
   val streamingDataFrame = spark.readStream
     .schema(staticSchema)
     .option("maxFilesPerTrigger", 1)
@@ -41,26 +50,32 @@ object Main extends App {
 
   println("is streaming " + streamingDataFrame.isStreaming)
 
-  val purchaseByCustomerPerHour = streamingDataFrame .selectExpr(
+  //streamingDataFrame.printSchema()
+
+  val purchaseByCustomerPerHour = streamingDataFrame.selectExpr(
     "CustomerId",
     "(UnitPrice * Quantity) as total_cost", "InvoiceDate")
     .groupBy(
-      col("CustomerId"), window(col("InvoiceDate"), "1 day"))
+      col("CustomerId"), window(col("InvoiceDate"), "10 minutes"))
     .sum("total_cost")
 
-  purchaseByCustomerPerHour.writeStream
+ /* purchaseByCustomerPerHour.writeStream
     .format("memory") // memory = store in-memory table
     .queryName("customer_purchases") // the name of the in-memory table
     .outputMode("complete") // complete = all the counts should be in the table
-    .start
+    .start*/
 
 
-  for (i <- 1 to 50) {
+
+  /*for (i <- 1 to 50) {
   spark.sql("""
       SELECT *
-      FROM customer_purchases_2
+      FROM customer_purchases
       ORDER BY `sum(total_cost)` DESC """)
-    .show(5, false)
-    Thread.sleep(3000)
-  }
+    .show( false)
+    Thread.sleep(500)
+  }*/
+
+
+
 }
